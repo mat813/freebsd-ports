ddc264ff2a9e74c5863fe15bfc291a6c753f5c09
Mathieu Arnold <mat@FreeBSD.org>
2019-01-11 14:40:47 +0100

    SC2223: This default assignment may cause DoS due to globbing. Quote it.
    This statement is an idiomatic way of assigning a default value to an
    environment variable. However, even though it's passed to : which
    ignores arguments, it's better to quote it.
    
    If COLUMNS='/*/*/*/*/*/*', the unquoted, problematic code may spend 30+
    minutes trashing the disk as it unnecessarily tries to glob expand the
    value.
    
    The correct code uses double quotes to avoid glob expansion, and
    therefore does not have this problem.

--- Mk/Scripts/dialog4ports.sh
+++ Mk/Scripts/dialog4ports.sh
@@ -10,8 +10,8 @@ if [ -z "${DIALOG4PORTS}" ] || [ -z "${PORTSDIR}" ] || [ -z "${MAKE}" ]; then
 	exit 1
 fi
 
-: ${DIALOGPORT:=ports-mgmt/dialog4ports}
-: ${DIALOGNAME:=dialog4ports}
+: "${DIALOGPORT:=ports-mgmt/dialog4ports}"
+: "${DIALOGNAME:=dialog4ports}"
 
 OPTIONSFILE="$1"
 
